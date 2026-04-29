e956f8bbe76b6c8115dd3ad18cf2007c38592be7
Mahmoud El-Naggar <mahmouddnnaggar@gmail.com>
2026-04-29 16:26:44 +0300

    add database port to db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,9 +7,10 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    logging: false
-  }
+    logging: false,
+  },
 );
 
 module.exports = sequelize;
