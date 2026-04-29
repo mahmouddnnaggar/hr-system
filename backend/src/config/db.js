f777f98397035962ae79104ab61be45f2eab723f
Mahmoud El-Naggar <mahmouddnnaggar@gmail.com>
2026-04-29 16:36:38 +0300

    update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'hr_evaluation_system',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',
-    logging: false,
-  },
-);
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  logging: false,
+});
 
 module.exports = sequelize;
